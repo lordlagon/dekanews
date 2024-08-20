94cb4c43ff568fddfb3dddaee6ba315e2bef8d39
Andre Macedo <andredekamacedo@gmail.com>
2024-08-20 00:04:07 -0300

    commit inicial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace DekaNews.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext(options)
+{
+    private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddJsonConsole());
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseLoggerFactory(_logger)
+            .UseSqlServer("Server=localhost,1433;Database=DekaNews;User Id=SA;Password=SU2orange;Encrypt=False",
+                p=> p
+                    .EnableRetryOnFailure(
+                        maxRetryCount:2, 
+                        maxRetryDelay: TimeSpan.FromSeconds(5), 
+                        errorNumbersToAdd:null)
+                    .MigrationsHistoryTable("DekaNews")
+            );
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
