393315ff8bfe36e73257946b07f4541cc023b82f
André <andredekamacedo@gmail.com>
2024-08-21 17:13:04 -0300

    inclusão da viewmodel

---
+++ ViewModels/NoticiaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DekaNews.ViewModels;
+
+public class NoticiaViewModel
+{
+    public string Titulo { get; set; }
+    public string Texto { get; set; }    
+    public MultiSelectList Tags { get; set; }
+}
