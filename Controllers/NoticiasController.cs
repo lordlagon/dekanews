7407e8c05ee2c184d3ad14e7f8ce2b3c4487ccd4
André <andredekamacedo@gmail.com>
2024-08-20 23:32:33 -0300

    inclusão das tags e noticias

---
+++ Controllers/NoticiasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DekaNews.Data;
+using DekaNews.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace DekaNews.Controllers
+{
+    public class NoticiasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NoticiasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Noticias
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Noticias.Include(c => c.Usuario);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Noticias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+
+            return View(noticia);
+        }
+
+        // GET: Noticias/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Noticias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Texto")] Noticia noticia)
+        {   
+            noticia.UsuarioId = User.Claims.FirstOrDefault()!.Value; 
+            if (ModelState.IsValid)
+            {
+                _context.Add(noticia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(noticia);
+        }
+
+        // GET: Noticias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias.FindAsync(id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+            return View(noticia);
+        }
+
+        // POST: Noticias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Texto")] Noticia noticia)
+        {
+            if (id != noticia.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(noticia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoticiaExists(noticia.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(noticia);
+        }
+
+        // GET: Noticias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+
+            return View(noticia);
+        }
+
+        // POST: Noticias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var noticia = await _context.Noticias.FindAsync(id);
+            if (noticia != null)
+            {
+                _context.Noticias.Remove(noticia);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoticiaExists(int id)
+        {
+            return _context.Noticias.Any(e => e.Id == id);
+        }
+    }
+}
